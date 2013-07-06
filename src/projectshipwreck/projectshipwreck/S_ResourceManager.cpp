9395cf01eddb2f7d598de22702c9e85898955f07
MatthiasHumt <matthias.humt@gmail.com>
2013-07-06 11:33:45 +0200

    Resourcemanager cleanup Funktion

--- src/projectshipwreck/projectshipwreck/S_ResourceManager.cpp
+++ src/projectshipwreck/projectshipwreck/S_ResourceManager.cpp
@@ -1,51 +1,57 @@
 #include "S_ResourceManager.h"
 using namespace ProjectShipwreckHighClass;
 
-void S_ResourceManager::LoadImage(string key, string filename)
-{
+void S_ResourceManager::LoadImage(e_Image key, string filename) {
 	SDL_Surface* loadedImage = NULL;
 	SDL_Surface* optimizedImage = NULL;
 
 	try {
 		loadedImage = IMG_Load(filename.c_str());
-	}
-	catch (exception loadIMG) {
-		cout << "<RessourceManager::LoadImage>: wrong filename" << endl;
-	}
+		if(loadedImage == NULL) {
+			throw "<RessourceManager::LoadImage>: cannot find image";
+		}
 
-	try {
 		optimizedImage = SDL_DisplayFormat(loadedImage);
+		if(optimizedImage == NULL) {
+			throw "<RessourceManager::LoadImage>: cannot optimize image";
+		}
 	}
-	catch (exception optimizeIMG) {
-		cout << "<RessourceManager::LoadImage>: error optimizing image" << endl;
+	catch (char* strException) {
+		cerr << "Error: " << strException << endl;
 	}
 
 	this->Pictures.insert(make_pair(key, optimizedImage));
 }
 
 
-void S_ResourceManager::LoadMusic(string key, string filename) {
+void S_ResourceManager::LoadMusic(e_Audio key, string filename) {
 	Mix_Music* loadedMusic = NULL;
 
 	try {
 		loadedMusic = Mix_LoadMUS(filename.c_str());
+		if(loadedMusic == NULL) {
+			throw "<RessourceManager::LoadMusic>: cannot find soundfile";
+		}
 	}
-	catch (exception loadMusic) {
-		cout << "<RessourceManager::LoadMusic>: error loading music" << endl;
+	catch (char* strException) {
+		cerr << "Error: " << strException << endl;
 	}
 
 	this->Music.insert(make_pair(key, loadedMusic));
 }
 
 
-void S_ResourceManager::LoadSound(string key, string filename) {
+void S_ResourceManager::LoadSound(e_Audio key, string filename) {
 	Mix_Chunk* loadedSound = NULL;
 
 	try {
 		loadedSound = Mix_LoadWAV(filename.c_str());
+		if(loadedSound == NULL) {
+			throw "<RessourceManager::LoadSound>: cannot find soundfile";
+		}
 	}
-	catch (exception loadSound) {
-		cout << "<RessourceManager::LoadSound>: error loading sound" << endl;
+	catch (char* strException) {
+		cerr << "Error: " << strException << endl;
 	}
 
 	this->Sounds.insert(make_pair(key, loadedSound));
@@ -53,41 +59,82 @@ void S_ResourceManager::LoadSound(string key, string filename) {
 
 
 void S_ResourceManager::LoadFont(string filename, int size) {
-	this->font = TTF_OpenFont(filename.c_str(), size);
+	try {
+		this->font = TTF_OpenFont(filename.c_str(), size);
+		if(font == NULL) {
+			throw "<RessourceManager::LoadFont>: cannot find font";
+		}
+	}
+	catch (char* strException) {
+		cerr << "Error: " << strException << endl;
+	}
 }
 
 
-void S_ResourceManager::ApplyPicture(int positionX, int positionY, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip)
-{
+void S_ResourceManager::ApplyPicture(int positionX, int positionY, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip) {
 	SDL_Rect Offset; // Used to store the x in y position in a SDL_Rect because SDL_BlitSurface only takes SDL_Rect
 
 	Offset.x = positionX;
 	Offset.y = positionY;
 
 	try {
-		SDL_BlitSurface(source, clip, destination, &Offset); // Show the loaded picture on the screen
+		if(SDL_BlitSurface(source, clip, destination, &Offset) == -1) { // Show the loaded picture on the screen
+			throw "<RessourceManager::ApplySurface>: unable to blit surface";
+		} 
 	}
-	catch (exception blitting) {
-		cout << "<RessourceManager::ApplySurface>: error blitting surface" << endl;
+	catch (char* strException) {
+		cerr << "Error: " << strException << endl;
 	}
 }
 
 
-void S_ResourceManager::LoadContent()
-{
-	cout << "Loading images..." << endl;
-
-	//LoadImage();
+void S_ResourceManager::LoadContent() {
+	cout << "Loading pictures..." << endl;
+	LoadImage(key_background, "background.jpg");
 
 	cout << "Loading music..." << endl;
-
 	//LoadMusic();
 
 	cout << "Loading sound files..." << endl;
-
 	//LoadSound();
 
 	cout << "Loading fonts..." << endl;
-
 	//LoadFont();
+}
+
+
+void S_ResourceManager::Clean() {
+	if(this->Pictures.size() > 0) {
+		for(this->pictures_it = this->Pictures.begin(); this->pictures_it != this->Pictures.end(); this->pictures_it++) {
+			SDL_FreeSurface(this->pictures_it->second);
+		}
+	}
+
+	if(this->Music.size() > 0) {
+		for(this->music_it = this->Music.begin(); this->music_it != Music.end(); this->music_it++) {
+			Mix_FreeMusic(this->music_it->second);
+		}
+	}
+
+	if(this->Sounds.size() > 0) {
+		for(this->sounds_it = this->Sounds.begin(); this->sounds_it != Sounds.end(); this->sounds_it++) {
+			Mix_FreeChunk(this->sounds_it->second);
+		}
+	}
+
+	this->Pictures.clear();
+	this->Music.clear();
+	this->Sounds.clear();
+
+	//Close the font
+    TTF_CloseFont( font );
+    
+    //Quit SDL_mixer
+    Mix_CloseAudio();
+    
+    //Quit SDL_ttf
+    TTF_Quit();
+    
+    //Quit SDL
+    SDL_Quit();
 }
--- src/projectshipwreck/projectshipwreck/S_ResourceManager.h
+++ src/projectshipwreck/projectshipwreck/S_ResourceManager.h
@@ -20,71 +20,74 @@ using namespace std;
 
 namespace ProjectShipwreckHighClass {
 
-	class S_ResourceManager
-	{
+	class S_ResourceManager {
+	public:
+		// Key enums
+		enum e_Image {
+			key_background
+		};
+
+		enum e_Audio {
+			key_temp
+		};
+
 	private:
 		// Pictures
 		SDL_Surface * screen;	// A pointer to the SDL_Surface that saves the screen
-		unordered_map<string, SDL_Surface*> Pictures;	// Used to get pictures with a keyword
+		unordered_map<e_Image, SDL_Surface*> Pictures;	// Used to get pictures with a keyword
+		unordered_map<e_Image, SDL_Surface*>::iterator pictures_it;
 
 		// Sounds
-		unordered_map<string, Mix_Music*> Music;
-		unordered_map<string, Mix_Chunk*> Sounds;
+		unordered_map<e_Audio, Mix_Music*> Music;
+		unordered_map<e_Audio, Mix_Chunk*> Sounds;
+		unordered_map<e_Audio, Mix_Music*>::iterator music_it;
+		unordered_map<e_Audio, Mix_Chunk*>::iterator sounds_it;
 
 		// Fonts
 		TTF_Font* font;
 
+		
+
+
 		S_ResourceManager()
 		{
-			// Try to initialize SDL
+			// Try to initialize everything
 			try {
-				SDL_Init(SDL_INIT_EVERYTHING);
-			}
-			catch (exception sdlINIT) {
-				cout << "<S_ResourceManager::Constructor>: error setting up SDL" << endl;
-			}
+				if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+					throw "<S_ResourceManager::Constructor>: SDL could not be initialized";
+				}
 
 			//Initialize SDL_ttf
-			try {
-				TTF_Init();
-			}
-			catch (exception sdlTTF) {
-				cout << "<S_ResourceManager::Constructor>: error setting up TTF" << endl;
-			}
+				if(TTF_Init() == -1) {
+					throw "<S_ResourceManager::Constructor>: SDL_ttf could not be initialized";
+				}
 
-			// Set color of SDL_Font
-			SDL_Color textColor = { 255, 255, 255 };
+				// Set color of SDL_Font
+				SDL_Color textColor = { 255, 255, 255 };
 
 			//Initialize SDL_mixer
-			try {
-				Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096);
-			}
-			catch (exception sdlMixer) {
-				cout << "<S_ResourceManager::Constructor>: error setting up sound" << endl;
-			}
-   
+				if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+					throw "<S_ResourceManager::Constructor>: SDL_mixer could not be initialized";
+				}
+	 
+				SDL_WM_SetCaption("projectshipwreck", NULL); // Name that is displayed in game window
 
-			SDL_WM_SetCaption("projectshipwreck", NULL); // Name that is displayed in game window
-
-			// Try to get the screen
-			try {
+				// Try to get the screen
 				this->screen = SDL_SetVideoMode(0, 0, 32, SDL_SWSURFACE | SDL_RESIZABLE);
+				if(this->screen == NULL) {
+					throw "<S_ResourceManager::Constructor>: SDL_SetVideoMode returned NULL";
+				}
 			}
-			catch (exception getScreen) {
-				cout << "S_ResourceManager::Constructor: error getting screen" << endl;
+			catch (char* strException) {
+				cerr << "Error: " << strException << endl;
+				exit(1);
 			}
-
-			cout << "S_ResourceManager constructed" << endl;
 		}
 
 	public:
 		~S_ResourceManager()
 		{
-			this->Pictures.clear();
-			this->Music.clear();
-			this->Sounds.clear();
-
-			cout << "S_ResourceManager destroyed" << endl;
+			this->Clean();
 		}
 
 
@@ -95,9 +98,9 @@ namespace ProjectShipwreckHighClass {
 		}
 
 
-		void LoadImage(string key, string filename);
-		void LoadSound(string key, string filename);
-		void LoadMusic(string key, string filename);
+		void LoadImage(e_Image key, string filename);
+		void LoadSound(e_Audio key, string filename);
+		void LoadMusic(e_Audio key, string filename);
 		void LoadFont(string filename, int size);
 		void ApplyPicture(int positionX, int positionY, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL);
 		void LoadContent();		// Call once at the beginning to load everything
@@ -105,8 +108,11 @@ namespace ProjectShipwreckHighClass {
 		void UpadteScreen() { SDL_Flip(this->screen); }		// Call after any picture is changed
 
 		SDL_Surface* GetScreen() const { return this->screen; }
-		void SetScreen(SDL_Surface* screen) { this->screen = screen; }
+		void SetScreen(SDL_Surface* temp_screen) { this->screen = temp_screen; }
 
-		SDL_Surface* GetPicture(string key) { return this->Pictures.at(key); }
+		SDL_Surface* GetPicture(e_Image key) { return this->Pictures.at(key); }
+		Mix_Chunk* GetSound(e_Audio key) { return this->Sounds.at(key); }
+		Mix_Music* GetMusic(e_Audio key) { return this->Music.at(key); }
+		TTF_Font* GetFont() { return this->font; }
 	};
 }
